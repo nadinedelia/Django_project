414c3eb3e4e3f333924788118b352f402b65e8bc
nadinedelia <nadine.delia@yahoo.com>
2020-08-12 20:18:53 +0200

    looping over dummy data, display on blog

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'NadineL',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 12, 2020'
+    },
+    {
+        'author': 'Elon Musk',
+        'title': 'Blog Post 12',
+        'content': 'Second post content',
+        'date_posted': 'August 13, 2020'
+    },
+]
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-     return HttpResponse('<h1>Blog About</h1>')
+     return render(request, 'blog/about.html')
