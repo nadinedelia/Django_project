74eb5cfccd00749daf2bdb7295425ddb34e07567
nadinedelia <nadine.delia@yahoo.com>
2020-08-16 19:37:59 +0200

    login required to view profile

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm 
 
 def register(request):
@@ -15,6 +16,6 @@ def register(request):
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
 
-
+@login_required
 def profile(request):
     return render(request, 'users/profile.html')
