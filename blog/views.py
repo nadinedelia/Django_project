eb5fe8b046d3d9e6e4ae58aacbb90cc0b0b63d42
nadinedelia <nadine.delia@yahoo.com>
2020-08-17 17:09:50 +0200

    blog post can be updated

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
+    UpdateView,
 )
 from .models import Post
 
@@ -34,6 +35,13 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
