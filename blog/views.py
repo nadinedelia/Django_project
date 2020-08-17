3e27fe71d829d58031f7c7fc7ed9e8900c48ae2f
nadinedelia <nadine.delia@yahoo.com>
2020-08-17 16:53:18 +0200

    route for new post

--- blog/views.py
+++ blog/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from django.views.generic import (
     ListView,
     DetailView,
+    CreateView,
 )
 from .models import Post
 
-# Create your views here.
+
 def home(request):
     context = {
         'posts': Post.objects.all()
@@ -15,13 +16,23 @@ def home(request):
 
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html'
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
 
 class PostDetailView(DetailView):
     model = Post
 
 
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
 def about(request):
-     return render(request, 'blog/about.html', {'title': 'About'})
+    return render(request, 'blog/about.html', {'title': 'About'})
