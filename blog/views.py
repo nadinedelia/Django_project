f49c11fabed1217e1acbd7c12b6d2ae96b7c7adb
nadinedelia <nadine.delia@yahoo.com>
2020-08-18 16:49:01 +0200

    corrected typos, user page now working properly, displaying only post of certain user

--- blog/views.py
+++ blog/views.py
@@ -33,9 +33,9 @@ class UserPostListView(ListView):
     ordering = ['-date_posted']
     paginate_by = 5
 
-    def get_query_set(self):
+    def get_queryset(self):
         user = get_object_or_404(User, username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).ordery_by('-date_posted')
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
